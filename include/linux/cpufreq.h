b8326bdd1a42fa61f269aff4f2e21a20913c78ea
Zachariah <zkennedy87@gmail.com>
2016-02-10 22:27:01 +0000

    SmartMAX: Import and setup for N6!
    * Converted to use PowerSuspend
    * Removed Boosting, we use CPU-Boost
    
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    # Conflicts:
    #	drivers/cpufreq/Kconfig
    #	drivers/cpufreq/Makefile
    #	include/linux/cpufreq.h
    
    (cherry picked from commit e4cc6ea00f6e9b62799d52ce5407c6f38ddfc4a5)
    
    # Conflicts:
    #	drivers/cpufreq/Kconfig
    #	drivers/cpufreq/Makefile
    #	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -473,6 +473,9 @@ extern struct cpufreq_governor cpufreq_gov_despair;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_NIGHTMARE)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
 #endif
 
 /*********************************************************************
